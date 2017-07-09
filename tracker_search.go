6618784ca7794f76b003f19914f119c3b8f0704f
Vladimir Barmotin <barmotinvladmimir@gmail.com>
2017-07-09 21:58:22 +0600

    Added json mapping

--- tracker_search.go
+++ tracker_search.go
@@ -3,6 +3,6 @@ package torrent_center
 import "github.com/bamboV/torrent"
 
 type TrackerSearchResult struct {
-	Name string
-	Items []torrent.Distribution
+	Name string `json:"name"`
+	Items []torrent.Distribution `json:"items"`
 }
