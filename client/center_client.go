eaa0b3c59ce02ce8100200c7f19e6556aa27b78e
Vladimir Barmotin <barmotinvladmimir@gmail.com>
2017-08-01 13:57:20 +0600

    Fixed pointer bugs

--- client/center_client.go
+++ client/center_client.go
@@ -16,11 +16,11 @@ type CenterClient struct {
 	CenterURL string
 }
 
-type magnet struct {
+type magnetType struct {
 	magnet string
 }
 
-func (c *CenterClient) GetDistribution(trackerName string, id int) (torrent.Distribution, error) {
+func (c *CenterClient) GetDistribution(trackerName string, id int) (*torrent.Distribution, error) {
 	resp, err := c.Client.Get(c.CenterURL+"/distributions/" + trackerName + "/" + strconv.Itoa(id))
 
 	if err != nil {
@@ -35,7 +35,7 @@ func (c *CenterClient) GetDistribution(trackerName string, id int) (torrent.Dist
 
 	}
 
-	return tr, nil
+	return &tr, nil
 }
 
 func (c *CenterClient) Search(trackerName string, phrase string) ([]torrent.Distribution, error) {
@@ -74,7 +74,7 @@ func (c *CenterClient) SearchInAllTrackers(phrase string) ([]torrent_center.Trac
 	return result, nil
 }
 
-func (c *CenterClient) GetTracker(trackerName string) (torrent_center.Tracker, error) {
+func (c *CenterClient) GetTracker(trackerName string) (*torrent_center.Tracker, error) {
 	resp, err := c.Client.Get(c.CenterURL + "/trackers/" + trackerName)
 
 	if err != nil {
@@ -89,7 +89,7 @@ func (c *CenterClient) GetTracker(trackerName string) (torrent_center.Tracker, e
 		return nil, err
 	}
 
-	return result, nil
+	return &result, nil
 }
 
 func (c *CenterClient) GetTrackers() ([]torrent_center.Tracker, error) {
@@ -110,7 +110,7 @@ func (c *CenterClient) GetTrackers() ([]torrent_center.Tracker, error) {
 	return result, nil
 }
 
-func (c *CenterClient) CreateTracker(tracker torrent_center.Tracker) (torrent_center.Tracker, error) {
+func (c *CenterClient) CreateTracker(tracker torrent_center.Tracker) (*torrent_center.Tracker, error) {
 	str, _ := json.Marshal(tracker)
 	resp, err := c.Client.Post(c.CenterURL + "/trackers", "application/json", bytes.NewBuffer(str))
 
@@ -126,10 +126,10 @@ func (c *CenterClient) CreateTracker(tracker torrent_center.Tracker) (torrent_ce
 		return nil, err
 	}
 
-	return result, nil
+	return &result, nil
 }
 
-func (c *CenterClient) UpdateTracker(tracker torrent_center.Tracker) (torrent_center.Tracker, error) {
+func (c *CenterClient) UpdateTracker(tracker torrent_center.Tracker) (*torrent_center.Tracker, error) {
 	str, _ := json.Marshal(tracker)
 	req, _ := http.NewRequest(http.MethodPut, c.CenterURL + "/trackers", bytes.NewBuffer(str))
 	resp, err := c.Client.Do(req)
@@ -146,11 +146,11 @@ func (c *CenterClient) UpdateTracker(tracker torrent_center.Tracker) (torrent_ce
 		return nil, err
 	}
 
-	return result, nil
+	return &result, nil
 }
 
 func (c *CenterClient) Download(magnet string) bool {
-	str, _ := json.Marshal(magnet{magnet:magnet})
+	str, _ := json.Marshal(magnetType{magnet:magnet})
 
 	resp, err := c.Client.Post(c.CenterURL + "/magnet", "application/json", bytes.NewBuffer(str))
 
